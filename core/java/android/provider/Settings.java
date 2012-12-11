eede1e091fd32691d2e84040e3641065d57d0edc
Roman Birg <romanbirg@gmail.com>
2012-12-11 04:12:41 -0600

    SystemUI: add left and right virtual buttons while typing
    Adapt for cm-12.0.
    
    Squashes the following commits:
    
        Fix side button visibility tracking.
    
        Previously one could e.g. get legacy menu button being visible in
        certain scenarios, like this one:
        - Open legacy app, e.g. CatLog
        - Invoke IME
        - Lock screen
        - Unlock screen
        - Result: Menu button visible in keyguard
    
        Change-Id: Ia796f73ec995ea1cc34d38f8aefdbccda7be7565
    
        SystemUI: fix up navigation cursor keyevent flags
    
        Volume cursor controls use slightly different flags to keep the cursor
        within the field. Match them in the SystemUI for consistent (and better)
        behavior.
    
        Change-Id: I56d597f111ec611b0ca24a10f30575401fa76d18
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
        SystemUI: fix up initial cursor down flags on nav bar
    
        We were using the regular flags for the initial ACTION_DOWN event so the
        FLAG_KEEP_TOUCH_MODE was not being passed and allowed the cursor to jump
        out of bounds slightly.
    
        Change-Id: I5b4d2a5e5e0ce562985fcc3b7b6fac8b4abc49da
        Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Change-Id: Iea446443d33ff305765e47c86523b619676116ac

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3809,6 +3809,13 @@ public final class Settings {
         public static final String GESTURE_ANYWHERE_SHOW_TRIGGER = "gesture_anywhere_show_trigger";
 
         /**
+         * boolean value. toggles using arrow key locations on nav bar
+         * as left and right dpad keys
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_MENU_ARROW_KEYS = "navigation_bar_menu_arrow_keys";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
