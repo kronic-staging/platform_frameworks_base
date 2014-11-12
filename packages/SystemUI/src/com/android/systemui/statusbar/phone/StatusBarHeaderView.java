c6b755f4795a0d9e7fa13a3680cdb21835d5760d
kaiyiz <kaiyiz@codeaurora.org>
2014-11-12 17:13:56 +0800

    Fix user profile activity overlapped by status bar panel issue
    Should collapse panels after start user profile activity.
    
    Use startActivity() of PhoneStatusBar class instead of
    startActivityAsUser() to collapse panels.
    
    Change-Id: I950349471ac63b7ad57903670cc659f3e5d21705
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -347,6 +347,9 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     public void setActivityStarter(ActivityStarter activityStarter) {
         mActivityStarter = activityStarter;
         mWeatherDetailed.setActivityStarter(activityStarter);
+        if (mMultiUserSwitch != null) {
+            mMultiUserSwitch.setActivityStarter(activityStarter);
+        }
     }
 
     public void setBatteryController(BatteryController batteryController) {
