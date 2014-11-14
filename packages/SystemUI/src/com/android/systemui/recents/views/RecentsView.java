e2bbdba504328fec84d9c3705d83dbb0f252642e
Clark Scheff <clark@cyngn.com>
2014-11-14 09:44:51 -0800

    Add clear all recents action to recents panel
    Change-Id: I2243ebc83abc483e78b112900a653d7e61edc5ba
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/recents/RecentsActivity.java
    	packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -198,6 +198,17 @@ public class RecentsView extends FrameLayout implements TaskStackView.TaskStackV
         }
     }
 
+    public void dismissAllTasksAnimated() {
+        int childCount = getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = getChildAt(i);
+            if (child != mSearchBar) {
+                TaskStackView stackView = (TaskStackView) child;
+                stackView.dismissAllTasks();
+            }
+        }
+    }
+
     /** Launches the focused task from the first stack if possible */
     public boolean launchFocusedTask() {
         // Get the first stack view
