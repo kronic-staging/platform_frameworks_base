04b33d39493454b48c6fbbc38f6803a0b6135ded
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:48:03 +0000

    QS: add Screen off tile
    * Works with SystemUI tuner
    * Adapted for M by KreAch3R
    * Long press for power menu
    
    Drawables by Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: Ib40968ed38edeb26fa657c69106d446c6ebdb39e
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/minimal_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -42,6 +42,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
+import com.android.systemui.qs.tiles.ScreenOffTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -267,6 +268,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("location")) return new LocationTile(this);
         else if (tileSpec.equals("cast")) return new CastTile(this);
         else if (tileSpec.equals("hotspot")) return new HotspotTile(this);
+        else if (tileSpec.equals("screenoff")) return new ScreenOffTile(this);
         else if (tileSpec.equals("timeout")) return new ScreenTimeoutTile(this);
         else if (tileSpec.equals("usb_tether")) return new UsbTetherTile(this);
         else if (tileSpec.equals("ambient_display")) return new AmbientDisplayTile(this);
