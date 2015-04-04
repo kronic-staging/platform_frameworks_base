4fee6653a7b70fa09dcf0d7e8ac14a5d8ab83a15
Dave Kessler <activethrasher00@gmail.com>
2015-04-04 15:47:24 +0200

    Add membar to recents options [1/2]
    Change-Id: Id1e799729543e385767ec8b68b126a20c4874baf
    
    Conflicts:
    	packages/SystemUI/res/layout/recents_membar.xml
    	packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3693,7 +3693,7 @@ public final class Settings {
         */
         public static final String RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
 
-	/**
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
