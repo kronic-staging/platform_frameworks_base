a608c1912299d32e352cb1d7655e0cfadc23cc59
Yoshinori Hirano <yoshinori.hirano@sonymobile.com>
2015-04-07 18:41:51 +0900

    Add Sound tile to Quick Settings
    Add Sound tile to make user easier to change sound mode
    to manner mode.
    
    Change-Id: I2debaa48a8809505a390b20fd4b67499583d2048
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -47,6 +47,7 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.OwlsNestTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
 import com.android.systemui.qs.tiles.ScreenOffTile;
 import com.android.systemui.qs.tiles.ScreenTimeoutTile;
@@ -288,6 +289,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("music")) return new MusicTile(this);
         else if (tileSpec.equals("brightness")) return new BrightnessTile(this);
         else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(this);
+        else if (tileSpec.equals("sound")) return new SoundTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -213,6 +213,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("music")) return R.string.quick_settings_music_label;
         else if (spec.equals("brightness")) return R.string.quick_settings_brightness;
         else if (spec.equals("expanded_desktop")) return R.string.quick_settings_expanded_desktop;
+        else if (spec.equals("sound")) return R.string.quick_settings_sound_label;
         return 0;
     }
 
@@ -466,6 +467,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("music")) return R.drawable.ic_qs_media_play;
             else if (mSpec.equals("brightness")) return R.drawable.ic_qs_brightness_auto_on_alpha;
             else if (mSpec.equals("expanded_desktop")) return R.drawable.ic_qs_expanded_desktop;
+            else if (mSpec.equals("sound")) return R.drawable.ic_qs_ringer_audible;
             return R.drawable.android;
         }
 
