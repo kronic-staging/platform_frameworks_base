0be22b1d81d46d95dfb66e0de03c53a00f23ce6b
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-07-19 20:18:56 +0200

    SlimDim: Use semaphore to cancel ontouch callback
    Only cancel callback if there actually is a callback pending
    
    PS3 : forgot to set semaphore to false in the runnable, once it's
          calledback, it's not scheduled anymore, no need to cancel it.
    
    Change-Id: I205ccadb4afcf0f8ef4157b666d0df6161132c91
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -89,6 +89,8 @@ public class NavigationBarView extends LinearLayout {
         }
     };
 
+    private boolean mIsHandlerCallbackActive = false;
+
     final Display mDisplay;
     View mCurrentView = null;
     View[] mRotatedViews = new View[4];
@@ -227,7 +229,10 @@ public class NavigationBarView extends LinearLayout {
     };
 
     public void onNavButtonTouched() {
-        mHandler.removeCallbacks(mNavButtonDimmer);
+        if (mIsHandlerCallbackActive) {
+            mHandler.removeCallbacks(mNavButtonDimmer);
+            mIsHandlerCallbackActive = false;
+        }
         final boolean keyguardProbablyEnabled =
                 (mDisabledFlags & View.STATUS_BAR_DISABLE_HOME) != 0;
         if (getNavButtons() != null) {
@@ -240,6 +245,7 @@ public class NavigationBarView extends LinearLayout {
             }
             if (mDimNavButtons && !keyguardProbablyEnabled) {
                 mHandler.postDelayed(mNavButtonDimmer, mDimNavButtonsTimeout);
+                mIsHandlerCallbackActive = true;
             }
         }
     }
@@ -1039,6 +1045,7 @@ public class NavigationBarView extends LinearLayout {
                     UserHandle.USER_CURRENT);
             mDimNavButtonsTouchAnywhere = (Settings.System.getIntForUser(resolver,
                     Settings.System.DIM_NAV_BUTTONS_TOUCH_ANYWHERE, 0,
+                    UserHandle.USER_CURRENT) == 1);
             mDoubleTapToSleep = (Settings.System.getIntForUser(resolver,
                     Settings.System.DOUBLE_TAP_SLEEP_NAVBAR, 0,
                     UserHandle.USER_CURRENT) == 1);
@@ -1047,6 +1054,7 @@ public class NavigationBarView extends LinearLayout {
 
     private Runnable mNavButtonDimmer = new Runnable() {
         public void run() {
+            mIsHandlerCallbackActive = false;
             if (getNavButtons() != null && mIsDim == false) {
                 mIsDim = true;
                 if (mDimNavButtonsAnimate) {
