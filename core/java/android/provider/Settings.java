876fdf6b930a234667a59a033ff5861ff35a52ae
DvTonder <david.vantonder@gmail.com>
2015-07-25 10:34:53 -0400

    Frameworks: Allow/Prevent notification light in Zen mode (1 of 2) This allows the user to prevent the notification lights from showing during Zen mode as per Android 5.0.x

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3995,6 +3995,12 @@ public final class Settings {
         public static final String RECENT_SHOW_RUNNING_TASKS = "show_running_tasks";
 
         /**
+         * Whether the notification light will be allowed when in zen mode during downtime
+         * @hide
+         */
+        public static final String ALLOW_LIGHTS = "allow_lights";
+
+        /**
          * Amount of apps to show in recents
          * @hide
          */
@@ -4203,7 +4209,8 @@ public final class Settings {
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
             STATUS_BAR_SHOW_BATTERY_PERCENT,
-            STATUS_BAR_BATTERY_STYLE
+            STATUS_BAR_BATTERY_STYLE,
+            ALLOW_LIGHTS
         };
 
         /**
