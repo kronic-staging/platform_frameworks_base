1128ed420a02ec9c7dbc167f3ac473d73377b5fa
kufikugel <kufikugel@googlemail.com>
2015-11-29 05:51:19 +0000

    Add Music QS Tile
    picked from here: http://goo.gl/ehaoMU
    
    Adapted to lollipop from commit:
    Author: kufikugel
    Quicksettings customizations Slim style
    d85304fac9bde550beeeeca0dba2cd49686f20d7
    
    Adapted double click to open music app from commit:
    Author: Jubakuba
    SystemUI: Pimp-up Media Tile (1/1)
    5a14de5db237969607e0aabb9b8c04737b122dd3
    
    AICPfy:
    PS1: Gave proper credit to kufikugel for the original code
    PS2: Fix build by overriding all needed methids
    
    DU edits (I'm sure the benzo fuck head will delete)
    - Adapted to M and made to work with AOSP Quick settings
    
    Change-Id: I4d3008d3dcfdf56f1cef25a602a153264c00c80f
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/du_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -40,6 +40,7 @@ import com.android.systemui.qs.tiles.HeadsUpTile;
 import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
+import com.android.systemui.qs.tiles.MusicTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.OwlsNestTile;
 import com.android.systemui.qs.tiles.RebootTile;
@@ -282,6 +283,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("owls_nest")) return new OwlsNestTile(this);
         else if (tileSpec.equals("battery_saver")) return new BatterySaverTile(this);
         else if (tileSpec.equals("reboot")) return new RebootTile(this);
+        else if (tileSpec.equals("music")) return new MusicTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -210,6 +210,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("owls_nest")) return R.string.quick_settings_owls_nest;
         else if (spec.equals("battery_saver")) return R.string.quick_settings_battery_saver;
         else if (spec.equals("reboot")) return R.string.quick_settings_reboot_label;
+        else if (spec.equals("music")) return R.string.quick_settings_music_label;
         return 0;
     }
 
@@ -460,6 +461,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("owls_nest")) return R.drawable.ic_settings_illusion;
             else if (mSpec.equals("battery_saver")) return R.drawable.ic_qs_battery_saver_on;
             else if (mSpec.equals("reboot")) return R.drawable.ic_qs_reboot;
+            else if (mSpec.equals("music")) return R.drawable.ic_qs_media_play;
             return R.drawable.android;
         }
 
