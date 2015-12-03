650cc371f6a76f1502d3aff296b18c03fd4477a7
maxwen <max.weninger@gmail.com>
2015-12-03 05:33:05 -0500

    [1/2] base: unlink notification volume (Squashed)
    because of omni merging 6.0 into 5.1 just cherrypicking their commit wasnt gonna work, and had undesired consequences this squashs all the relevant parts needed for 6.0
    
    Squashed By: @BeansTown106

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3177,6 +3177,12 @@ public final class Settings {
         public static final Validator LOCKSCREEN_SOUNDS_ENABLED_VALIDATOR = sBooleanValidator;
 
         /**
+         * Boolean value whether to link ringtone and notification volume
+         * @hide
+         */
+        public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
+
+        /**
          * Whether the lockscreen should be completely disabled.
          * @hide
          */
