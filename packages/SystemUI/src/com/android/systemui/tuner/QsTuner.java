30eae6498eb76fec45be1253845c032cd08093c9
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-12-09 13:39:59 +0000

    Add Battery Saver QS Tile
    - Drawables by Alexander Westphal <westphal.alex94@gmail.com>
    - Adapted to M/AOSP Quick Settings by @Mazda--
    
    Change-Id: I5dc94435e138d08bde747e3f477d1117147c83e7
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/du_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
    
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -208,6 +208,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("headsup")) return R.string.quick_settings_heads_up_label;
         else if (spec.equals("sync")) return R.string.quick_settings_sync_label;
         else if (spec.equals("owls_nest")) return R.string.quick_settings_owls_nest;
+        else if (spec.equals("battery_saver")) return R.string.quick_settings_battery_saver;
         return 0;
     }
 
@@ -456,6 +457,7 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("headsup")) return R.drawable.ic_qs_heads_up_on;
             else if (mSpec.equals("sync")) return R.drawable.ic_qs_sync_on;
             else if (mSpec.equals("owls_nest")) return R.drawable.ic_settings_illusion;
+            else if (mSpec.equals("battery_saver")) return R.drawable.ic_qs_battery_saver_on;
             return R.drawable.android;
         }
 
