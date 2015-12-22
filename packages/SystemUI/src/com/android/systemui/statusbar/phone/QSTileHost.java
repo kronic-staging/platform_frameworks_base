ada4d9b06aff38a46e7f2fa43b523e7b54e9bb94
Alex Cruz <mazdarider23@gmail.com>
2015-12-22 13:31:32 -0800

    QS: NavBar tile
    Benzo Rom Edits:
    Made to work with M and SystemUI Tuner
    
    Conflicts:
    	core/java/com/android/internal/logging/MetricsConstants.java
    	packages/SystemUI/res/values/config.xml
    
    Conflicts:
    	core/java/com/android/internal/logging/MetricsConstants.java
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.HotspotTile;
 import com.android.systemui.qs.tiles.IntentTile;
 import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.MusicTile;
+import com.android.systemui.qs.tiles.NavBarTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.OwlsNestTile;
 import com.android.systemui.qs.tiles.RebootTile;
@@ -288,6 +289,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("music")) return new MusicTile(this);
         else if (tileSpec.equals("brightness")) return new BrightnessTile(this);
         else if (tileSpec.equals("expanded_desktop")) return new ExpandedDesktopTile(this);
+        else if (tileSpec.equals("navbar")) return new NavBarTile(this);
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
         else throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
     }
