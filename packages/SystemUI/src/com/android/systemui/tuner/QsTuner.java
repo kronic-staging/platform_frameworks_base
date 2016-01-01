258f8afecddb921a8fba4972ff42757728924ce4
Danesh M <daneshm90@gmail.com>
2016-01-01 10:13:09 +0000

    Add adb over network QS Tile
    Conflicts:
    	packages/SystemUI/res/values/config.xml
    	packages/SystemUI/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    	packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java

--- packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
+++ packages/SystemUI/src/com/android/systemui/tuner/QsTuner.java
@@ -217,6 +217,7 @@ public class QsTuner extends Fragment implements Callback {
         else if (spec.equals("sound")) return R.string.quick_settings_sound_label;
         else if (spec.equals("kernel")) return R.string.quick_settings_kernel_adiutor_label;
         else if (spec.equals("themes")) return R.string.quick_settings_themes;
+        else if (spec.equals("adb_network")) return R.string.quick_settings_adb_network;
         return 0;
     }
 
@@ -472,8 +473,10 @@ public class QsTuner extends Fragment implements Callback {
             else if (mSpec.equals("expanded_desktop")) return R.drawable.ic_qs_expanded_desktop;
             else if (mSpec.equals("navbar")) return R.drawable.ic_qs_navbar;
             else if (mSpec.equals("sound")) return R.drawable.ic_qs_ringer_audible;
+<<<<<<< HEAD
             else if (mSpec.equals("kernel")) return R.drawable.ic_qs_kernel;
 	    else if (mSpec.equals("themes")) return R.drawable.ic_qs_themes_on;
+            else if (mSpec.equals("adb_network")) return R.drawable.ic_qs_network_adb_on;
             return R.drawable.android;
         }
 
