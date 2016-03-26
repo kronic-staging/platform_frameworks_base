f7cb38c3c3b03bf0ffee9bafcea1c0c3c81c5b5c
BigBrother1984 <carlosavignano@aospa.co>
2016-03-26 16:55:22 -0400

    Improve Heads up toggle [1/2] * Add multiuser support, provide a system setting to override heads up   showing. User can disable heads up within this one so we don't need to   mess with Settings.Global heads up dependencies and break Google wide   implementations like priority filtering etc. *updated heads up quicksetting tile to reflect changes as well
    PureNexus Edits:
    *Adapted for Marshmallow/Nougat
    
    Change-Id: I09aeba75d7d0f0b90ba58d6f99b4bcf7395624c6

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -212,6 +212,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     private float mFontScale;
 
     protected boolean mUseHeadsUp = false;
+    protected boolean mHeadsUpUserEnabled = false;
     protected boolean mHeadsUpTicker = false;
     protected boolean mDisableNotificationAlerts = false;
 
@@ -2675,7 +2676,7 @@ public abstract class BaseStatusBar extends SystemUI implements
     }
 
     protected boolean shouldPeek(Entry entry, StatusBarNotification sbn) {
-        if (!mUseHeadsUp || isDeviceInVrMode()) {
+        if (!mUseHeadsUp || !mHeadsUpUserEnabled || isDeviceInVrMode()) {
             return false;
         }
 
