35ea93acdbc87468369a55185d034df55bb1f7c2
Varun Date <date.varun123@gmail.com>
2016-04-27 23:37:41 +0530

    FWB:Dyanamically Configurable Pulse Bar Dimensions [1/3]
    Signed-off-by: Varun Date <date.varun123@gmail.com>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -657,6 +657,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 	    resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NAVBAR_BUTTON_COLOR),
                     false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.PULSE_CUSTOM_DIMEN),
+                    false, this, UserHandle.USER_ALL);
 		    update();
         }
 
@@ -785,6 +788,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 	   } else if (uri.equals(Settings.System.getUriFor(
                 Settings.System.NAVBAR_BUTTON_COLOR))) {
    	        mNavigationController.updateNavbarOverlay(getNavbarThemedResources());
+	   } else if (uri.equals(Settings.System.getUriFor(
+                Settings.System.PULSE_CUSTOM_DIMEN))) {
+ 	        mNavigationController.updateNavbarOverlay(getNavbarThemedResources());
 		} 
             update();
         }
