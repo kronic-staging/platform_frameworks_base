32c53c8059b2da5c7b98a4252c8418c6a09fa41f
Alex Cruz <mazdarider23@gmail.com>
2016-06-23 21:31:41 -0400

    Lockscreen Clock Widget Fonts [1/2] Squashed with: *Lockscreen fonts: set correct default value -@BeansTown106 *Update Lockscreen clock font option -@BeansTown106 *fix lockscreen font resetting on reboot -@BeansTown106
    PureNexus Edits:
    *Squashed
    *Modified for use with our observer
    *Cleaned up if statements a bit saved about 15 LOC
    
    Change-Id: I4078570e78ec1b912f6f61ae04d7093715f41515

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3906,12 +3906,6 @@ public final class Settings {
          */
         public static final String HIDE_LOCKSCREEN_CLOCK = "hide_lockscreen_clock";
 
-        /**
-         * Hide lockscreen date
-         * @hide
-         */
-        public static final String HIDE_LOCKSCREEN_ALARM = "hide_lockscreen_alarm";
-
        /**
         * MediaScanner behavior on boot.
         * 0 = enabled
@@ -4089,6 +4083,30 @@ public final class Settings {
          * @hide
          */
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
+
+        /**
+         * show lockscreen alarm
+         *  @hide
+         */
+        public static final String SHOW_LOCKSCREEN_ALARM = "show_lockscreen_alarm";
+
+        /**
+         * show lockscreen clock
+         *  @hide
+         */
+        public static final String SHOW_LOCKSCREEN_CLOCK = "show_lockscreen_clock";
+
+        /**
+         * show lockscreen date
+         *  @hide
+         */
+        public static final String SHOW_LOCKSCREEN_DATE = "show_lockscreen_date";
+
+        /**
+         * Change fonts for the system lockscreen clock widget
+         * @hide
+         */
+        public static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
 
         /**
          * Toast icon
