34fd7da8072696aece248e290beac924ef4c4201
BeansTown106 <nbenis106@gmail.com>
2016-09-24 19:58:49 -0700

    QS: Add toggle for fancy QS animation [1/2]
    Change-Id: I330fd5963a04ee6ab893cfc7093749cb76ff5596

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4165,6 +4165,12 @@ public final class Settings {
         public static final String DIALER_OPENCNAM_AUTH_TOKEN = "dialer_opencnam_auth_token";
 
         /**
+         * Quick Settings nougat animation
+         * @hide
+         */
+        public static final String QS_FANCY_ANIMATION = "qs_fancy_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
