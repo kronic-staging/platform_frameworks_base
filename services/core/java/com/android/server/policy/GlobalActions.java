30ab171dfe0caf5a7c4e349746f17bdc5917762f
LorDClockaN <davor@losinj.com>
2016-09-28 20:34:18 +0200

    Power/reboot dialogs dim [1/2]
    - Controls the dim amount of the power/reboot menu dialog
    
    Change-Id: Ibecc51ce8ecc6c338abf94a698b8e167dff541df

--- services/core/java/com/android/server/policy/GlobalActions.java
+++ services/core/java/com/android/server/policy/GlobalActions.java
@@ -264,6 +264,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             ((SinglePressAction) mAdapter.getItem(0)).onPress();
         } else {
             mDialog.getWindow().setAttributes(attrs);
+            mDialog.getWindow().setDimAmount(setPowerRebootDialogDim());
             mDialog.show();
             mDialog.getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_DISABLE_EXPAND);
         }
@@ -272,6 +273,14 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
     private int getPowermenuAnimations() {
         return Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.POWER_MENU_ANIMATIONS, 0);
+    }
+
+    private float setPowerRebootDialogDim() {
+        int mPowerRebootDialogDim = Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.POWER_REBOOT_DIALOG_DIM, 50);
+        double dDim = mPowerRebootDialogDim / 100.0;
+        float dim = (float) dDim;
+        return dim;
     }
 
     /**
--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -303,6 +303,7 @@ public final class ShutdownThread extends Thread {
                 attrs.windowAnimations = R.style.PowerMenuTranslucentAnimation;
                 attrs.gravity = Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL;
             }
+            sConfirmDialog.getWindow().setDimAmount(setPowerRebootDialogDim(context));
             sConfirmDialog.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
             sConfirmDialog.show();
         } else {
@@ -350,6 +351,14 @@ public final class ShutdownThread extends Thread {
         public void onDismiss(DialogInterface unused) {
             mContext.unregisterReceiver(this);
         }
+    }
+
+    private static float setPowerRebootDialogDim(Context context) {
+        int mPowerRebootDialogDim = Settings.System.getInt(context.getContentResolver(),
+                Settings.System.POWER_REBOOT_DIALOG_DIM, 50);
+        double dDim = mPowerRebootDialogDim / 100.0;
+        float dim = (float) dDim;
+        return dim;
     }
 
     /**
@@ -506,6 +515,7 @@ public final class ShutdownThread extends Thread {
             attrs.gravity = Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL;
         }
 
+        pd.getWindow().setDimAmount(setPowerRebootDialogDim(context));
         pd.show();
 
         sInstance.mProgressDialog = pd;
