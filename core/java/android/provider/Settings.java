8095fa801a29a203fb1965333eb330b2cae7e5ad
Evisceration <eviscerationls@gmail.com>
2016-09-29 05:11:26 +0200

    Add Haptic Feedback to QS tiles [1/2]
    Change-Id: If358fb624032cd24d98fe2ea0c877f4875c5c151

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3966,6 +3966,13 @@ public final class Settings {
         public static final String LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
 
         /**
+         * Whether to vibrate when quick settings tile is pressed.
+         *
+         * @hide
+         */
+        public static final String QUICK_SETTINGS_TILES_VIBRATE = "quick_settings_vibrate";
+
+        /**
          * Whether shorcuts open with normal or longpress
          * @hide
          */
