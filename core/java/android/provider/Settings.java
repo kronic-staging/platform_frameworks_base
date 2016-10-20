e06745cbb32bf657b5711e5d05ae380a36022758
Martinusbe <martinusbe@gmail.com>
2016-10-20 10:38:07 +0200

    Visualizer: take it out off CM
    aosip edit: lets make this a Secure Setting
    
    Change-Id: I387af09326738161c7222b458a2280e245ac1470
    Signed-off-by: Martinusbe <martinusbe@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -7121,6 +7121,13 @@ public final class Settings {
          */
         public static final String QS_WIFI_EASY_TOGGLE = "qs_wifi_easy_toggle";
 
+       /**
+         * Whether to show the keyguard visualizer.
+         * Boolean setting. 0 = off, 1 = on.
+         * @hide
+         */
+        public static final String LOCKSCREEN_VISUALIZER_ENABLED = "lockscreen_visualizer";
+
         /**
          * Whether the device should doze if configured.
          * @hide
