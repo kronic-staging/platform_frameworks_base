148312d767f683395d80000d1684853286a7913c
ezio84 <brabus84@gmail.com>
2017-03-24 10:15:03 +0100

    Build Slim Recents into SystemUI [1/2]
    track packages/apps/SlimRecents tree and pick the Settings commits
    
    Change-Id: I1eaa2342d7e29cfd7f983f169ff1937bfc37fdb2

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -2523,6 +2523,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
             case TYPE_STATUS_BAR_PANEL:
             case TYPE_STATUS_BAR_SUB_PANEL:
             case TYPE_SYSTEM_DIALOG:
+            case TYPE_SLIM_RECENTS:
             case TYPE_VOLUME_OVERLAY:
             case TYPE_PRIVATE_PRESENTATION:
             case TYPE_DOCK_DIVIDER:
@@ -2698,6 +2699,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         case TYPE_INPUT_CONSUMER:
             return 6;
         case TYPE_SYSTEM_DIALOG:
+        case TYPE_SLIM_RECENTS:
             return 7;
         case TYPE_TOAST:
             // toasts and the plugged-in battery thing
