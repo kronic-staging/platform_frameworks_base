27e75654351304d7989e2e413e4e24bb98d2619c
xlxfoxxlx <joshfox87@gmail.com>
2017-04-02 22:51:42 -0400

    Fix npe from derp in slimrecents
    Change-Id: I3150a235cbbd3a2333fa75a80fd1bd99684d98c6

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -62,8 +62,8 @@ LOCAL_RESOURCE_DIR := \
     frameworks/support/v7/cardview/res
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
-    --extra-packages com.android.keyguard:android.support.v7.recyclerview:android.support.v7.preference:android.support.v14.preference:android.support.v7.appcompat 
-    --extra-packages android.support.v17.leanback:android.support.v7.cardview
+    --extra-packages com.android.keyguard:android.support.v7.recyclerview:android.support.v7.preference:android.support.v14.preference:android.support.v7.appcompat \
+	--extra-packages android.support.v17.leanback:android.support.v7.cardview
 
 ifneq ($(SYSTEM_UI_INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
